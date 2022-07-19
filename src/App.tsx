c9db2ca68ffc7d02ce24193f24e91a77c8079291
progmamun <mamun333089@gmail.com>
2022-07-19 22:54:42 +0600

    task-add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
+import { ITask } from './Interfaces';
 
 const App: FC = () => {
   const [task, setTask] = useState<string>('');
   const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === 'task') {
@@ -12,6 +14,14 @@ const App: FC = () => {
       setDeadline(Number(event.target.value));
     }
   };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask('');
+    setDeadline(0);
+  };
+
   return (
     <div className='App'>
       <div className='header'>
